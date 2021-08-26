b88e6a4a5f838f14fe7469d5d2bac6351b066c74
Thomas Eding <teding@groq.com>
2021-08-26 09:53:51 -0700

    wip

--- setup/groq/ubuntu-remote/main.bash
+++ setup/groq/ubuntu-remote/main.bash
@@ -1,7 +1,14 @@
-# download and install deja vu fonts
-# install iterm2 and import json profile
+# download and install deja vu fonts on host
+# install iterm2 and import json profile on host
 # fix git config: name, email, git behaviors
 # add vm public rsa key to github
+
+# On host ~/.ssh/config, add:
+# SendEnv TMUX_SESSION
+# To one of the sections.
+
+# sudo vi /etc/ssh/sshd_config
+# Then add anywhere in file: AcceptEnv TMUX_SESSION
 
 cd "$HOME"
 
@@ -22,5 +29,14 @@ build-hs () {
 
 build-hs ansi2prompt
 build-hs substr
+
+# Check /etc/sysctl.conf to see if
+#     fs.inotify.max_user_watches = 524288
+# is set to that max value first
+sudo sysctl -p
+
+# https://apple.stackexchange.com/a/55886/46550
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+test -f ~/.git-completion.bash && . $_
 
 
